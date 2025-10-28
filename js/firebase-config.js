03466380e564d64cc4c60a0d0f71357c048c75b2
Thalita Costa <thalita.martins@estudante.iftm.edu.br>
2025-10-28 18:43:06 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,3 @@
-// firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
